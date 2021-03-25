79cb115195c8fa0d4ed9dcbb1b8cbcc3bbb9d808
RustCerebri <klopixol89@gmail.com>
2021-03-25 09:43:07 +0300

    delete images

--- src/app/modules/gallery/gallery.component.ts
+++ src/app/modules/gallery/gallery.component.ts
@@ -1,7 +1,7 @@
 import { SavedImg } from './../../interfaces/savedImg.interface';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { GalleryService } from './../services/gallery.service';
-import { Component, OnInit } from "@angular/core";
+import { Component, DoCheck, OnInit } from "@angular/core";
 import { take } from 'rxjs/operators';
 
 
@@ -11,15 +11,18 @@ import { take } from 'rxjs/operators';
   styleUrls: ['./gallery.component.scss']
 })
 
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements OnInit, DoCheck {
 
   public allImages: SavedImg[] = [];
   public currentIndex: number = 0;
-  // private currentImages: string = this.allImages[this.currentIndex].id;
+  public array: SavedImg[] = [];
+  public isShow: boolean;
 
   constructor(private galleryService: GalleryService, private fireAuth: AngularFireAuth) {}
 
   public ngOnInit(): void {
+
+
     this.fireAuth.user.subscribe(res => {
       if(res) {
         this.galleryService.getPublishedImages(res.uid)
@@ -28,13 +31,20 @@ export class GalleryComponent implements OnInit {
         )
         .subscribe(images => {
             this.allImages = images;
-            // console.log(this.allImages[this.currentIndex].id);
-        })
+            this.condition();
+         })
       }
     })
   }
 
+  public ngDoCheck(): void {
+    this.condition()
+  }
+
+
+
   public removeImage (id: string): void {
+    id = this.allImages[this.currentIndex].id;
     this.galleryService.remove(id).pipe(take(1)).subscribe(()=>{
       this.allImages = this.allImages.filter(post => post.id !== id)
     })
@@ -51,10 +61,22 @@ export class GalleryComponent implements OnInit {
 
   public prevImg(): void {
     if(this.currentIndex === 0) {
-      this.currentIndex= this.allImages.length-1;
+      this.currentIndex = this.allImages.length-1;
     } else {
       this.currentIndex--;
     }
+   }
+
+  public condition(): void {
+         if (this.allImages.length === this.array.length) {
+       this.isShow = true;
+       this.allImages = this.array
+     }
+     else {
+       this.isShow = false;
+     }
+
+
   }
 
 
